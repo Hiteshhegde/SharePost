fa801f676f91e3b2ceedd024f1b12ce1be0b469f
Hiteshhegde <hiteshhegde95@gmail.com>
2021-08-26 16:30:29 -0700

    Added forms.py

--- NewPro/blog/forms.py
+++ NewPro/blog/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 
 #Create your forms here
+
+#Login form
 class LoginForm(forms.Form):
     user_name= forms.CharField(label='User name',max_length= 220)
     password = forms.CharField(label='Password',max_length=50)
     remember_me = forms.BooleanField(label='remember me ?')
 
+#Registeration Form
 class RegisterForm(forms.Form):
     user_name= forms.CharField(label='Username', max_length=220)
     password = forms.CharField(label='Password', max_length=50)
